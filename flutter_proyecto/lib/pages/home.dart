f38f38f66407891e7ad60e0f823e520d239c9154
Andre391 <150189948+Andre391@users.noreply.github.com>
2024-03-26 12:00:18 -0500

    Clase 26 de Marzo Flutter, mejora del home

--- flutter_proyecto/lib/pages/home.dart
+++ flutter_proyecto/lib/pages/home.dart
@@ -4,11 +4,10 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.blueGrey.shade100,
+      backgroundColor: const Color.fromARGB(255, 249, 235, 163),
       appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.blueAccent,
@@ -18,13 +17,27 @@ class HomePage extends StatelessWidget {
           children: [
             Container(
               margin:const EdgeInsets.only(top:30, bottom: 50) ,
-              child: Icon(Icons.wb_sunny_outlined,size: 120, color: Colors.greenAccent,),
+              child: const Icon(Icons.wb_sunny_outlined,size: 120, color: Color.fromARGB(255, 138, 79, 209),),
             ),
-              Expanded(
-            child:Card(child:ListView(shrinkWrap: true,padding: const EdgeInsets.all(15),children: [
+              Card(
+                color:const Color.fromARGB(255, 246, 219, 109),
+                margin: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+              child:ListView(shrinkWrap: true,padding: const EdgeInsets.all(15),children: [
                Container(
                 height:50 ,
                 margin: const EdgeInsets.only(top: 20, bottom: 27),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  gradient: const LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.topLeft,
+                     colors: [
+                     Color.fromARGB(255, 119, 200, 148),
+                     Color.fromARGB(255, 99, 181, 225),
+                    ],
+                  ),
+                 ),
                 child: const Center(
                   child: Text("OUR APP",
                   style: TextStyle(
@@ -34,10 +47,28 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                ),
+               OutlinedButton(
+                onPressed: (){},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 175, 142, 215),
+                  foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                  elevation: 5,
+                  side: const BorderSide(width: 2),
+                ), 
+                child: const Text("Boton"),),
+               OutlinedButton(
+                onPressed: (){},
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 215, 142, 206),
+                  foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+                  elevation: 5,
+                  side: const BorderSide(width: 2),
+                ), 
+                child: const Text("Boton"),
+                )
           ],
             ),
             ),
-              ),
           ],
  
           ),
